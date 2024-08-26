5a1f4f18f036e380820c2f02ea2138c59ec3188d
Mario Mori <mario_mori@hotmail.com>
2024-08-26 11:32:32 -0300

    added printing to sh script

--- tests-container.sh
+++ tests-container.sh
@@ -3,6 +3,8 @@
 if [[ $(docker ps | grep playwright-tests) != "" ]]
 then
     docker rmi $(docker images | grep playwright-tests | awk '{print $3}')
+    echo "Image deleted"
 else
+echo "Nothing to delete"
 exit 0
 fi
